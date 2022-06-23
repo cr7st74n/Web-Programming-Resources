13f42c16b4185b8a9263220fba56696082aa2abe
SSullivan <spencersullivan1990@gmail.com>
2022-06-23 14:32:55 -0500

    end of day save

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,10 @@
 // Write Your JavaScript Code Here
+
+var personName;
+var pets;
+
+personName='Spencer';
+pets='1';
+
+console.log('My name is ' + personName+'.');
+console.log('I have '+pets+' pet.')
